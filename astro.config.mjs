65ad5444bf844574d8894c40d974bd51ceb7ecb8
factu <factu@SISTEMAS>
2025-10-10 17:17:31 -0500

    Add Vercel adapter for serverless functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   // ¡LA LÍNEA QUE LO CAMBIA TODO!
   output: 'server',
-
+   adapter: vercel( ),
   integrations: [tailwind( )]
 });
